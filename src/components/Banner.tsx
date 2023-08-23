2bda69a56be70057f03d4612e903f5cf4832e498
Maekin <74455990+mekintown@users.noreply.github.com>
2023-08-23 15:22:12 +0700

    Setup project

---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+import styles from "@/components/card.module.css"
+
+const Banner = () => {
+    return (
+        <div className={styles.banner}>
+
+        </div>
+    )
+}
+
+export default Banner;
