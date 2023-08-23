3aab06303ff048c64f9110949f5870d6506ee7d9
Maekin <74455990+mekintown@users.noreply.github.com>
2023-08-23 20:22:11 +0700

    Add Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Banner from '@/components/Banner'
+import Card from '@/components/Card'
 
 export default function Home() {
   return (
     <main>
       <Banner/>
+      <Card imgSrc="/img/vaccination2.png" txt="Strengthen Your Immunity, Guard Your Loved Ones, and Contribute to a Healthier Community. Choose Protection, Choose Vaccination."/>
     </main>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,20 @@
-const Card = () => {
+import styles from "@/components/card.module.css"
+import Image from "next/image";
 
+type Props = {
+    imgSrc: string;
+    txt: string;
+};
+
+const Card = ({imgSrc, txt} : Props) => {
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardImg}>
+                <Image src={imgSrc} alt="vaccination" fill={true} objectFit="cover"/>
+            </div>
+            <p>{txt}</p>
+        </div>
+    )
 }
 
 export default Card;
