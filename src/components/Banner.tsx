c449bd3a371949768432b518f3762758bc6de6a4
Maekin <74455990+mekintown@users.noreply.github.com>
2023-08-28 18:21:58 +0700

    Edit banner design

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,13 +1,14 @@
-import styles from "@/components/banner.module.css"
 import Image from "next/image";
 
 const Banner = () => {
     return (
-        <div className={styles.banner}>
-            <Image src={"/img/cover.jpg"} alt="cover" fill={true} objectFit="cover"/>
-            <div className={styles.bannerTxt}>
-                <h1>Protect Yourself and Others – Get Vaccinated Today!"</h1>
-                <p>Elevate your health defenses with a simple yet powerful choice – vaccination. By getting vaccinated, you're not just shielding yourself from diseases, but also safeguarding your community. Our [Your Clinic/Hospital Name] experts are here to ensure you receive top-notch, safe vaccinations. Act now, prioritize health, and book your vaccination.</p>
+        <div className="grid grid-cols-3 bg-blue-400 p-10 min-h-[70vh]">
+            <div className="col-span-2 self-center flex flex-col gap-5">
+                <h1 className="font-bold text-6xl text-white tracking-wide">Protect Yourself and Others – Get Vaccinated Today!</h1>
+                <p className="text-white">Elevate your health defenses with a simple yet powerful choice – vaccination. By getting vaccinated, you're not just shielding yourself from diseases, but also safeguarding your community. Our [Your Clinic/Hospital Name] experts are here to ensure you receive top-notch, safe vaccinations. Act now, prioritize health, and book your vaccination.</p>
+            </div>
+            <div className="relative font-medium">
+                <Image src={"/img/vaccination.svg"} alt="cover" fill={true} objectFit="contain"/>
             </div>
         </div>
     )
