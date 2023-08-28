ece1ee4ae4124a1a82e1e836fe6c45a13861449c
Maekin <74455990+mekintown@users.noreply.github.com>
2023-08-28 18:24:34 +0700

    Edit banner text

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 const Banner = () => {
     return (
-        <div className="grid grid-cols-3 bg-blue-400 p-10 min-h-[70vh]">
+        <div className="grid grid-cols-3 bg-blue-400 p-10 min-h-[70vh] gap-10">
             <div className="col-span-2 self-center flex flex-col gap-5">
                 <h1 className="font-bold text-6xl text-white tracking-wide">Protect Yourself and Others – Get Vaccinated Today!</h1>
-                <p className="text-white">Elevate your health defenses with a simple yet powerful choice – vaccination. By getting vaccinated, you're not just shielding yourself from diseases, but also safeguarding your community. Our [Your Clinic/Hospital Name] experts are here to ensure you receive top-notch, safe vaccinations. Act now, prioritize health, and book your vaccination.</p>
+                <p className="text-white">Elevate your health defenses with a simple yet powerful choice – vaccination. By getting vaccinated, you're not just shielding yourself from diseases, but also safeguarding your community. Our experts are here to ensure you receive top-notch, safe vaccinations. Act now, prioritize health, and book your vaccination.</p>
             </div>
             <div className="relative font-medium">
                 <Image src={"/img/vaccination.svg"} alt="cover" fill={true} objectFit="contain"/>
